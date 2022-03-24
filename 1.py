d3ee7c6b82581f5305a6c746946698c13b9d9d51
baieji12 <18030448775@163.com>
2022-03-24 16:17:09 +0800

    作业

---
+++ 1.py
@@ -0,0 +1,13 @@
+res1 = 0
+for i in range(101):
+    if i % 2 == 0:
+        res1 += i
+print(res1)
+
+i = 1
+res2 = 0
+while i <= 100:
+    if i % 2 == 0:
+        res2 += i
+    i += 1
+print(res2)
